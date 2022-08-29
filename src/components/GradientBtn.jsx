f32486c52c45b6860ff1b42d564c1fefa37c3520
gift56 <efeasiughu@gmail.com>
2022-08-29 20:30:33 +0100

    stying button

--- src/components/GradientBtn.jsx
+++ src/components/GradientBtn.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const GradientBtn = ({ title, classNames }) => {
-  return <button>{title}</button>;
+  return (
+    <button className={`bg-gradient-to-r from-thOrange to-thBlue`}>
+      {title}
+    </button>
+  );
 };
 
 export default GradientBtn;
