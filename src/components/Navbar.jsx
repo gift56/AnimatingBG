58b7f63efb188402db353b6f6ddaf3c392f53f90
gift56 <efeasiughu@gmail.com>
2022-08-29 18:29:57 +0100

    created an arrays for links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
+  const Links = [
+    {
+      id: 1,
+      link:"home"
+    },
+    {
+      id: 2,
+      link:"products"
+    },
+    {
+      id: 3,
+      link:"developer api"
+    },
+    {
+      id: 4,
+      link:"customers"
+    },
+    {
+      id: 5,
+      link:"contact"
+    },
+  ];
   return (
-    <div className='absolute w-full h-24 bg-black text-white z-20'>
-        <div className='flex justify-between items-center max-w-screen-xl mx-auto px-4 h-full'>
-            <div>
-                <h1 className='text-thBlue tracking-widest text-3xl'>ANTON</h1>
-            </div>
+    <div className="absolute w-full h-24 bg-black text-white z-20">
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto px-4 h-full">
+        <div>
+          <h1 className="text-thBlue tracking-widest text-3xl">ANTON</h1>
         </div>
+        <div className="hidden md:flex items-center">
+          <ul className="flex"></ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
