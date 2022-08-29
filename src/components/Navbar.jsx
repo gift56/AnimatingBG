f539af0aa83f8af35e80f38f7f2640ae7a0b55bc
gift56 <efeasiughu@gmail.com>
2022-08-29 18:24:29 +0100

    done with logo text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='absolute w-full h-24 bg-black text-white z-20'>
+        <div className='flex justify-between items-center max-w-screen-xl mx-auto px-4 h-full'>
+            <div>
+                <h1 className='text-thBlue tracking-widest text-3xl'>ANTON</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
