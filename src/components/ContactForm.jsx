1ac57ba2628569d6277caa08656fc4273cb28b4e
gift56 <efeasiughu@gmail.com>
2022-08-30 15:51:20 +0100

    added label to contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,9 @@ const ContactForm = () => {
   return (
     <div className='p-4 text-left'>
         <form action="" method='POST'>
-
+            <div className='grid lg:grid-cols-2 gap-4 w-full py-2'>
+                
+            </div>
         </form>
     </div>
   )
