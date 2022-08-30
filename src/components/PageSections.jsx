1ebee1d203801f6160cdd2e1edf16b2190b1defc
gift56 <efeasiughu@gmail.com>
2022-08-30 11:46:12 +0100

    changed somethings

--- src/components/PageSections.jsx
+++ src/components/PageSections.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageSection = ({ name, title, subtitle, children }) => {
   return (
     <div name={name} className="max-w-screen-xl mx-auto h-fit">
-      <div className="my-32 mx-8 d-flex flex-col gap-4 items-center lg:items-start">
+      <div className="my-20 mx-8 d-flex flex-col gap-4 items-center lg:items-start">
         <h1 className="text-5xl">{title}</h1>
         <p className="mt-8 mb-16">{subtitle}</p>
         {children}
