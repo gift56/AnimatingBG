0e62c35cb349e5a21a5722df4c9c6498967cc873
gift56 <efeasiughu@gmail.com>
2022-08-30 16:26:10 +0100

    created footer items

---
+++ src/utils/footerData.js
@@ -0,0 +1,75 @@
+export const menu = [
+  {
+    id: 1,
+    title: "Company",
+    subMenu: [
+      {
+        id: 1,
+        title: "Submenu 1",
+      },
+      {
+        id: 2,
+        title: "Submenu 2",
+      },
+      {
+        id: 3,
+        title: "Submenu 3",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Support",
+    subMenu: [
+      {
+        id: 1,
+        title: "Apple 1",
+        link: "https://apple.com",
+      },
+      {
+        id: 2,
+        title: "Apple 2",
+      },
+      {
+        id: 3,
+        title: "Apple 3",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Developer API",
+    subMenu: [
+      {
+        id: 1,
+        title: "Grape 1",
+      },
+      {
+        id: 2,
+        title: "Grape 2",
+      },
+      {
+        id: 3,
+        title: "Grape 3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Stuff",
+    subMenu: [
+      {
+        id: 1,
+        title: "Stuff 1",
+      },
+      {
+        id: 2,
+        title: "Stuff 2",
+      },
+      {
+        id: 3,
+        title: "Stuff 3",
+      },
+    ],
+  },
+];
