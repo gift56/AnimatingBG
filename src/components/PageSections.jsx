b435a208f0d2245c7395b48062b2d57688789935
gift56 <efeasiughu@gmail.com>
2022-08-30 12:07:39 +0100

    added subtitle

--- src/components/PageSections.jsx
+++ src/components/PageSections.jsx
@@ -5,7 +5,7 @@ const PageSection = ({ name, title, subtitle, children }) => {
     <div name={name} className="max-w-screen-xl mx-auto h-fit">
       <div className="my-20 mx-8 d-flex flex-col gap-4 items-center lg:items-start">
         <h1 className="text-5xl">{title}</h1>
-        <p className="mt-8 mb-16">{subtitle}</p>
+        <p className="mt-3 mb-16">{subtitle}</p>
         {children}
       </div>
     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import PageSection from './PageSections'
+import React from "react";
+import PageSection from "./PageSections";
 
 const Products = () => {
   return (
-    <PageSection name="products" title="Products">
+    <PageSection
+      name="products"
+      title="Products"
+      subtitle={`Plainly lamplight whether evermore thereat tell, we or scarce bird spoken he whether on, sinking thy again sent what eyes tinkled his explore these. Within melancholy sad bird not respiterespite..`}
+    ></PageSection>
+  );
+};
 
-    </PageSection>
-  )
-}
-
-export default Products
+export default Products;
