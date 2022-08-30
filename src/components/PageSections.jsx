43af0322ee1723cf4f60c51a391d57dbda852695
gift56 <efeasiughu@gmail.com>
2022-08-30 11:14:08 +0100

    working with p tag

--- src/components/PageSections.jsx
+++ src/components/PageSections.jsx
@@ -4,8 +4,8 @@ const PageSections = ({ name, title, subtitle, children }) => {
   return (
     <div name={name} className="max-w-screen-xl mx-auto h-fit">
       <div className="my-32 mx-8 text-center lg:text-left">
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
+        <h1 className="text-5xl">{title}</h1>
+        <p className="mt-8 mb-16">{subtitle}</p>
         {children}
       </div>
     </div>
