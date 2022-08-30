1da8c09101807ad57e6a9c9a8182a7b5b591de42
gift56 <efeasiughu@gmail.com>
2022-08-30 15:32:23 +0100

    adding some stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PageSection from "./PageSections";
+import contactImg from "../assets/contactUs.jpg";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -7,7 +9,22 @@ const Contact = () => {
       name="contact"
       title="Contact"
       subtitle={`Nevermore outpour hesitating whispered all my demons, marvelled that bird said and meant back entreating dying. Parting the above broken.`}
-    ></PageSection>
+    >
+      <div className="flex flex-col lg:flex-row gap-8 mt-4">
+        <div className="w-full lg:w-1/2 h-full p-4 flex flex-col items-center justify-center">
+          <img
+            src={contactImg}
+            alt="contact us"
+            className="rounded-full object-cover !shadow-lg !shadow-thPurple"
+          />
+          <p>
+            Off what of no stronger that. Oh help this i tapping is door hear i.
+            Rare in the i tossed and him has, and hesitating i thy burned into
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </PageSection>
   );
 };
 
