08277cb3f8d6cd6b1611fa793de9f46e0d9c7142
gift56 <efeasiughu@gmail.com>
2022-08-30 11:09:09 +0100

    working with props

--- src/components/PageSections.jsx
+++ src/components/PageSections.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const PageSections = ({ name, title, subtitle, children }) => {
-  return <div>PageSections</div>;
+  return (
+    <div name={name} className="max-w-screen-xl mx-auto h-fit">
+      <div className="my-32 mx-8 text-center lg:text-left">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default PageSections;
