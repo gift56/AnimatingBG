e9b634997af20fcf95cd0addd5a9faeec6ca7bb1
gift56 <efeasiughu@gmail.com>
2022-08-30 16:25:24 +0100

    working on footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Contact from "./components/Contact";
 import Customers from "./components/Customers";
 import DeveloperApi from "./components/DeveloperApi";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
@@ -16,6 +17,7 @@ const App = () => {
       <DeveloperApi />
       <Customers />
       <Contact />
+      <Footer />
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="border-t border-white">
+        <div className="my-32 max-w-screen-xl mx-auto">
+            <div className="grid grid-cols-2 lg:grid-cols-4 text-center gap-12">
+                <div>
+                    <h1>Company</h1>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
