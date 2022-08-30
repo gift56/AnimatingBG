2803d728063d01faff4db9282ff571b742a8aa2b
gift56 <efeasiughu@gmail.com>
2022-08-30 15:03:59 +0100

    fixing some stuff

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -13,7 +13,7 @@ const Customers = () => {
         <div className="grid lg:grid-cols-4 text-left gap-8 mb-16">
           {facts.map(({ id, title, subtitle }) => (
             <div
-              className="odd:bg-thBlue even:bg-thOrange text-black p-4 rounded-lg"
+              className="odd:bg-thBlue even:bg-thOrange text-black p-4 rounded-lg w-[90vw] lg:w-full"
               key={id}
             >
               <h1 className="text-3xl font-bold mb-2">{title}</h1>
