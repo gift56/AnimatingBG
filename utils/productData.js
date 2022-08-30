c4e17929189c5e1a86148d3bee0956626c2fd04e
gift56 <efeasiughu@gmail.com>
2022-08-30 12:17:59 +0100

    created data to map through

---
+++ utils/productData.js
@@ -0,0 +1,30 @@
+export const products = [
+    {
+      id: 1,
+      icon: <FaRocket size={50} className="text-white" />,
+      title: "rocket manufacturing",
+      subtitle:
+        "Fact from with my the i, this shadows soul i mien was of bird, raven till mien before by of. The lordly one fancy me not and croaking smiling, but hear lamplight entrance tempest a name i, sitting gave name each the quoth my door his implore. I i nothing.",
+    },
+    {
+      id: 2,
+      icon: <FaMicrochip size={50} className="text-white" />,
+      title: "advanced electronics",
+      subtitle:
+        "Fact from with my the i, this shadows soul i mien was of bird, raven till mien before by of. The lordly one fancy me not and croaking smiling, but hear lamplight entrance tempest a name i, sitting gave name each the quoth my door his implore. I i nothing.",
+    },
+    {
+      id: 3,
+      icon: <FaRobot size={50} className="text-white" />,
+      title: "robotics",
+      subtitle:
+        "Fact from with my the i, this shadows soul i mien was of bird, raven till mien before by of. The lordly one fancy me not and croaking smiling, but hear lamplight entrance tempest a name i, sitting gave name each the quoth my door his implore. I i nothing.",
+    },
+    {
+      id: 4,
+      icon: <FaSatellite size={50} className="text-white" />,
+      title: "payload to orbit",
+      subtitle:
+        "Fact from with my the i, this shadows soul i mien was of bird, raven till mien before by of. The lordly one fancy me not and croaking smiling, but hear lamplight entrance tempest a name i, sitting gave name each the quoth my door his implore. I i nothing.",
+    },
+  ];
